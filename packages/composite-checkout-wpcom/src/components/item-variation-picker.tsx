8d142e9cf84f06fc197885d5c38dfbb852e85fde
nbloomf <nathan.bloomfield@automattic.com>
2020-01-23 17:14:08 -0600

    Add ItemVariationPicker component

---
+++ packages/composite-checkout-wpcom/src/components/item-variation-picker.tsx
@@ -0,0 +1,92 @@
+/**
+ * External dependencies
+ */
+import React, { Component, FunctionComponent } from 'react';
+import styled from '@emotion/styled';
+import { useTranslate } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import { WPCOMCartItem } from '../types';
+import RadioButton from './radio-button';
+
+export type WPCOMProductSlug = string;
+
+export type WPCOMProductVariant = {
+	variantLabel: string;
+	variantDetails: Component;
+	productSlug: WPCOMProductSlug;
+};
+
+export type ItemVariationPickerProps = {
+	selectedItem: WPCOMCartItem;
+	getVariants: ( WPCOMProductSlug ) => WPCOMProductVariant[];
+};
+
+export const ItemVariationPicker: FunctionComponent< ItemVariationPickerProps > = ( {
+	selectedItem,
+	getVariants,
+} ) => {
+	const selectedProductSlug = selectedItem.wpcom_meta.product_slug;
+	const variants = getVariants( selectedProductSlug );
+
+	console.log( variants );
+
+	if ( variants.length < 2 ) {
+		return false;
+	}
+
+	return <TermOptions>{ variants.map( renderProductVariant( selectedProductSlug ) ) }</TermOptions>;
+};
+
+const renderProductVariant: (
+	WPCOMProductSlug
+) => ( WPCOMProductVariant, number ) => Component = selectedProductSlug => (
+	{ variantLabel, variantDetails, productSlug },
+	index
+) => {
+	const translate = useTranslate();
+	const key = index.toString() + productSlug;
+
+	return (
+		<TermOptionsItem key={ key }>
+			<RadioButton
+				name={ key }
+				id={ key }
+				value={ productSlug }
+				checked={ productSlug === selectedProductSlug }
+				onChange={ () => {
+					console.log( 'Select:', variantLabel );
+				} }
+				ariaLabel={ translate( variantLabel ) }
+				label={
+					<React.Fragment>
+						<VariantLabel>{ translate( variantLabel ) }</VariantLabel>
+						{ variantDetails }
+					</React.Fragment>
+				}
+			/>
+		</TermOptionsItem>
+	);
+};
+
+const TermOptions = styled.ul`
+	flex-basis: 100%;
+	margin: 12px 0 0;
+	padding: 0;
+`;
+
+const TermOptionsItem = styled.li`
+	margin: 8px 0 0;
+	padding: 0;
+	list-style: none;
+
+	:first-of-type {
+		margin-top: 0;
+	}
+`;
+
+const VariantLabel = styled.span`
+	flex: 1;
+`;
